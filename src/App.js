8b5ac7e28084140b319db2d1a6c5850e69b87f80
unknown <matheusficher98@gmail.com>
2023-08-03 21:32:08 -0300

    Atualização de codigo para o aplicativo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+
+  const getWeather = () => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=760c45014cd8a7f41da25c78741db19a&units=metric&lang=pt_BR`)
+      .then((response) => {
+        setWeather(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+      const handleKeyPress = (event) => {
+        if (event.key === "Enter"){
+          getWeather();
+        }
+      }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div style={{textAlign: 'center'}}>
+      <img src="/favicon.png" alt="Logo" style={{ width: '150px' }}/> 
+      <header>
+        <h1>Previsão do Tempo</h1>
       </header>
+
+      <input
+        type="text"
+        placeholder="Digite a cidade"
+        onChange={(e) => setCity(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+
+      <button onClick={getWeather}>Obter previsão</button>
+      {weather && (
+        <div>
+          <h1>Clima em {weather.name}</h1>
+          <p>Temperatura: {weather.main.temp}°C</p>
+          <p>Umidade: {weather.main.humidity}%</p>
+          <p>Condição: {weather.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
+
 
 export default App;
